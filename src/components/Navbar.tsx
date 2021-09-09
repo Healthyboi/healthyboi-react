6d1583878572a1dc784f630627e91119e26311a0
Grandelisn <grandelisn@gmail.com>
2021-09-09 19:05:24 -0400

    routes configured

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../assets/images/logo.png";
+const NavBar = () => {
+  console.log("woo navbar is here");
+  return (
+    <div className="navbar">
+      <div className="nav-links">
+        <Link to="/workouts">Workout Tracker</Link>
+        {/*add the route to the workout tracker page*/}
+        {/* <Link>About</Link> */}
+        <img src={logo} alt="healthy boy" />
+        <Link className="title" to="/">
+          Project Healthyboi
+        </Link>
+      </div>
+    </div>
+  );
+};
+export default NavBar;
---
+++ src/components/WorkoutList.tsx
@@ -0,0 +1,9 @@
+const WorkoutList = () => {
+  console.log("workout list mounted");
+  return (
+    <div className="workout-list">
+      <p>WORKOUT LIST PLACEHOLDER</p>
+    </div>
+  );
+};
+export default WorkoutList;
