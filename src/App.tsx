00d707dc497a09c0b046bac039fa5e9c21cf619a
Grandelisn <grandelisn@gmail.com>
2021-09-14 18:16:03 -0400

    stopwatch finished

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import Homepage from "./components/HomePage";
 import WorkoutList from "./components/WorkoutList";
+import Stopwatch from "./components/StopWatch/Stopwatch";
 import { Route, Switch } from "react-router";
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <header className="App-header">
         <Navbar />
       </header>
+      <Stopwatch />
       <Switch>
         <Route path="/" exact component={Homepage} />
         <Route path="/workouts" component={WorkoutList} />
---
+++ src/components/StopWatch/Stopwatch.tsx
@@ -0,0 +1,78 @@
+import { useEffect, useState } from "react";
+import "./Stopwatch.css";
+import Timer from "./Timer";
+import ControlButtons from "./ControlButtons";
+import Button from "@material-ui/core/Button";
+import Snackbar from "@material-ui/core/Snackbar";
+import MuiAlert from "@material-ui/lab/Alert";
+
+function Alert(props) {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
+function StopWatch() {
+  const [isActive, setIsActive] = useState(false);
+  const [isPaused, setIsPaused] = useState(true);
+  const [time, setTime] = useState(0);
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    let interval: any = null;
+
+    if (isActive && isPaused === false) {
+      interval = setInterval(() => {
+        setTime((time) => time + 10);
+      }, 10);
+    } else {
+      clearInterval(interval);
+    }
+    return () => {
+      clearInterval(interval);
+    };
+  }, [isActive, isPaused]);
+
+  const handleStart = () => {
+    setIsActive(true);
+    setIsPaused(false);
+    setTimeout(() => {
+      handleReset();
+      setOpen(true);
+    }, 5000);
+  };
+
+  const handlePauseResume = () => {
+    setIsPaused(!isPaused);
+  };
+
+  const handleReset = () => {
+    setIsActive(false);
+    setTime(0);
+  };
+
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  return (
+    <div className="stop-watch">
+      <Timer time={time} />
+      <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity="error">
+          Fuck you!
+        </Alert>
+      </Snackbar>
+      <ControlButtons
+        active={isActive}
+        isPaused={isPaused}
+        handleStart={handleStart}
+        handlePauseResume={handlePauseResume}
+        handleReset={handleReset}
+      />
+    </div>
+  );
+}
+
+export default StopWatch;
